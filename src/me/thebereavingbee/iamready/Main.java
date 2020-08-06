3115d599b46b0b3aab3efca942aa603eb4d619f3
Alexander <adedood@gmail.com>
2020-08-05 17:18:08 -0700

    Removed excessive whitespace

--- src/me/thebereavingbee/iamready/Main.java
+++ src/me/thebereavingbee/iamready/Main.java
@@ -14,7 +14,6 @@ public class Main extends JavaPlugin {
 		//startup
 		//reloads
 		//enable
-		
 	}
 	
 	@Override
@@ -23,34 +22,26 @@ public class Main extends JavaPlugin {
 		// restart
 		// plugin restart
 	}
-
+	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (label.equalsIgnoreCase("iamready") == true) {
 			if(sender instanceof Player) {
 				//player
 				Player player = (Player) sender;
 				if(player.hasPermission("iamready.use") == true) {
-					player.sendMessage(ChatColor.YELLOW + "Thanks for reading the rules! You are now ready to play!");
-					
+					player.sendMessage(ChatColor.YELLOW + "Thanks for reading the rules! You are now ready to play!");	
 					Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user "+ player.getName() +" parent add rookie");
 					return true;
 				}
-				
 				player.sendMessage(ChatColor.YELLOW + "You've already read the rules! Good on you!");
-				
 				return true;
 			}
 			else{
 				//console
-				sender.sendMessage("Hey console! You're already op'd!");
-				
+				sender.sendMessage("Hey console! You're already op'd!");		
 				return true;
 			}
 		}
-		
-		
-		
 		return false;
 	}
-	
 }
